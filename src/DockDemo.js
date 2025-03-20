bd221e507f88fc0aac490c3ac47d2fb41a25171c
Aditya Suryawanshi <suryawanshiaditya29@gmail.com>
2025-03-20 12:42:56 +0530

    separate file for tabs

--- src/DockDemo.js
+++ src/DockDemo.js
@@ -6,39 +6,10 @@ import { Box, Paper, Typography, IconButton, Tabs, Tab } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import DraggableTab from './DraggableTabs';
 
 const ItemType = 'TAB';
 
-const DraggableTab = ({ tab, index, moveTab, closeTab }) => {
-  const [, ref] = useDrag({
-    type: ItemType,
-    item: { index },
-  });
-
-  const [, drop] = useDrop({
-    accept: ItemType,
-    hover: (draggedItem) => {
-      if (draggedItem.index !== index) {
-        moveTab(draggedItem.index, index);
-        draggedItem.index = index;
-      }
-    },
-  });
-
-  return (
-    <Box
-      ref={(node) => ref(drop(node))}
-      sx={{ display: 'flex', alignItems: 'center', cursor: 'move', padding: 0.5 }}
-    >
-      <Typography variant="body2" sx={{ mr: 1 }}>
-        {tab.title}
-      </Typography>
-      <IconButton size="small" onClick={() => closeTab(tab.id)}>
-        <CloseIcon color="error" fontSize="small" />
-      </IconButton>
-    </Box>
-  );
-};
 
 const TabDemo = () => {
   const [tabs, setTabs] = useState([{ id: 1, title: 'Tab 1', content: 'This is content of Tab 1' }]);
